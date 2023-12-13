68c3220d27448a9f20b9fa95ec5c7ea1f9645417
Ski <ski.stumb@gmail.com>
2023-12-13 20:36:58 +0200

    Cleaning

--- main.py
+++
@@ -1,8 +0,0 @@
-from openfx_api.OpenFxApi import OpenFxApi
-from openfx_api.NewsApi import AlphaVantageApi
-from infrastructure.instrument_collection import instrumentCollection
-
-if __name__ == '__main__':
-    api = OpenFxApi()
-    n_api = AlphaVantageApi()
-    print(api.get_candles_df(pair_name="EURUSD", count=-32, granularity="M1"))
